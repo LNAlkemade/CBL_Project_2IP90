d17c073cc4f995440b052ecfd68ecd1b1b7ebf34
Anouk Doelitzsch <anm.doelitzsch@gmail.com>
2023-10-12 20:32:53 +0200

    Made a start game button dat disappears when you click it. Still needs work to make it readable.

--- Button.java
+++ Button.java
@@ -1,16 +1,32 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.FlowLayout;;
  
-class SimpleButton{
- 
-   public static void main(String args[]){
- 
-       JFrame frame = new JFrame();
-       JButton button = new JButton("Button");
- 
-       frame.add(button);
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.setSize(400,400);
-       frame.setLocationRelativeTo(null);
-       frame.setVisible(true);
+class Elements{
+    public static void main(){  
+        JFrame frame = new JFrame("Yahtzee");  
+        JPanel panel = new JPanel();  
+        panel.setLayout(new FlowLayout());  
+        JButton button = new JButton();  
+        button.setText("Start Game");  
+        panel.add(button);  
+        frame.add(panel);  
+        frame.setSize(200, 300);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true);
+        
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // When the button is clicked, hide it
+                button.setVisible(false);
+            }
+        });
+    } 
+
+    public static void main(String[] args){
+        new Elements().main();
     }
 }
