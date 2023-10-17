f4b163f6bbb6506f39179f345244024f155fa8eb
Luke <l.n.alkemade@student.tue.nl>
2023-10-17 15:47:08 +0200

    Wisselen van speler en rollen van dice werkt

--- YahtzeeScorecardGUI.java
+++ YahtzeeScorecardGUI.java
@@ -7,14 +7,35 @@ public class YahtzeeScorecardGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 600);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(18, 3));
+        JPanel dicePanel = new JPanel();
+        dicePanel.setLayout(new FlowLayout());
+
+        JTextField[] diceFields = new JTextField[5];
+        JCheckBox[] checkBoxes = new JCheckBox[5];
+
+        for (int i = 0; i < 5; i++) {
+            diceFields[i] = new JTextField(2);
+            diceFields[i].setPreferredSize(new Dimension(50, 50));
+            checkBoxes[i] = new JCheckBox("Keep");
+            JPanel diceWithCheckbox = new JPanel();
+            diceWithCheckbox.setLayout(new BoxLayout(diceWithCheckbox, BoxLayout.Y_AXIS));
+            diceWithCheckbox.add(diceFields[i]);
+            diceWithCheckbox.add(checkBoxes[i]);
+            dicePanel.add(diceWithCheckbox);
+        }
+
+        JPanel buttonPanel = new JPanel();
+        JButton rollButton = new JButton("Roll the Dice");
+        buttonPanel.add(rollButton);
+
+        JPanel scorePanel = new JPanel();
+        scorePanel.setLayout(new GridLayout(19, 3));
 
         JLabel playerLabel = new JLabel("Player 1");
-        panel.add(new JLabel()); // An empty cell in the grid
-        panel.add(playerLabel);
+        scorePanel.add(new JLabel());
+        scorePanel.add(playerLabel);
         playerLabel = new JLabel("Player 2");
-        panel.add(playerLabel);
+        scorePanel.add(playerLabel);
 
         String[] categories = {
             "Points", "Ones", "Twos", "Threes", "Fours", "Fives", "Sixes",
@@ -23,24 +44,28 @@ public class YahtzeeScorecardGUI {
             "Grand Total"
         };
 
-        JTextField[][] scores = new JTextField[18][2];
+        JTextField[][] scores = new JTextField[19][2];
 
-        for (int i = 0; i < 17; i++) {
+        for (int i = 0; i < 18; i++) {
             JLabel label = new JLabel(categories[i]);
-            panel.add(label);
+            scorePanel.add(label);
 
             for (int j = 0; j < 2; j++) {
                 if (i == 7 || i == 8 || i == 16) {
                     scores[i][j] = new JTextField();
-                    scores[i][j].setEditable(false); // Make the text fields uneditable
+                    scores[i][j].setEditable(false);
                 } else {
                     scores[i][j] = new JTextField();
                 }
-                panel.add(scores[i][j]);
+                scorePanel.add(scores[i][j]);
             }
         }
 
-        frame.add(panel);
+        frame.setLayout(new BorderLayout());
+
+        frame.add(dicePanel, BorderLayout.NORTH);
+        frame.add(buttonPanel, BorderLayout.CENTER);
+        frame.add(scorePanel, BorderLayout.SOUTH);
         frame.setVisible(true);
     }
 }
